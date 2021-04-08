243e3cf6558b892c66461747850b79ea35e3ad9b
Wolf <kenmutati@gmail.com>
2021-04-08 13:28:54 +0300

    Python database models

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'the wolf',
-        'title': 'blog post 1',
-        'content': 'first post content',
-        'date_posted': 'March 23,2021'
-    },
-    {
-        'author': 'the wolf tech',
-        'title': 'blog post 2',
-        'content': 'second post content',
-        'date_posted': 'March 23,2021'
-    },
-]
+from .models import Post
 
 
 # Create your views here.
 def home(request):
-    context ={
-        'posts':posts
+    context = {
+        'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html',context)
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html',{'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
